43ddf63c47f0bd108e9004d60f15382fba9423b2
Assel1 <assel1@users.noreply.github.com>
2018-05-28 15:33:03 +0600

    спрайты и исправления после 1 проверки

--- source/js/script.js
+++ source/js/script.js
@@ -5,13 +5,13 @@ var menuButtonOpen= document.querySelector(".main-navigation__button-burger");
 var menuButtonClose= document.querySelector(".main-navigation__button-close");
 
 
-modalMenu.classList.remove("main-navigation__list-opened");
+modalMenu.classList.remove("main-navigation__list--opened");
 menuButtonBurger.classList.add("main-navigation__button-show");
 
 
 menuButtonOpen.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modalMenu.classList.add("main-navigation__list-opened");
+  modalMenu.classList.add("main-navigation__list--opened");
   menuButtonClose.classList.add("main-navigation__button-show");
   menuButtonBurger.classList.remove("main-navigation__button-show");
 });
@@ -20,5 +20,5 @@ menuButtonClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   menuButtonBurger.classList.add("main-navigation__button-show");
   menuButtonClose.classList.remove("main-navigation__button-show");
-  modalMenu.classList.remove("main-navigation__list-opened");
+  modalMenu.classList.remove("main-navigation__list--opened");
 });
