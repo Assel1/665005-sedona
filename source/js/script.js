50909c3233df9a60e6ae428b317e37b7262ec078
Assel1 <assel1@users.noreply.github.com>
2018-05-30 00:10:59 +0600

    toggle

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,8 @@
-var menu= document.querySelector("nav");
-var modalMenu= document.querySelector(".main-navigation__list");
-var menuButtonBurger= document.querySelector(".main-navigation__button-burger");
-var menuButtonOpen= document.querySelector(".main-navigation__button-burger");
-var menuButtonClose= document.querySelector(".main-navigation__button-close");
+var buttonToggle = document.querySelector(".main-navigation__toggle");
+var toggleMenu = document.querySelector(".main-navigation__list");
 
-
-modalMenu.classList.remove("main-navigation__list--opened");
-menuButtonBurger.classList.add("main-navigation__button-show");
-
-
-menuButtonOpen.addEventListener("click", function(evt) {
+buttonToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modalMenu.classList.add("main-navigation__list--opened");
-  menuButtonClose.classList.add("main-navigation__button-show");
-  menuButtonBurger.classList.remove("main-navigation__button-show");
-});
-
-menuButtonClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  menuButtonBurger.classList.add("main-navigation__button-show");
-  menuButtonClose.classList.remove("main-navigation__button-show");
-  modalMenu.classList.remove("main-navigation__list--opened");
+  buttonToggle.classList.toggle("main-navigation__toggle");
+  toggleMenu.classList.toggle("main-navigation__list_hidden");
 });
