62424df70ae53d1c6711534b0558da90529d95b6
Assel1 <assel1@users.noreply.github.com>
2018-05-30 15:54:14 +0600

    радиобаттон

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
-var buttonToggle = document.querySelector(".main-navigation__toggle");
+var buttonToggle = document.querySelector(".main-navigation__toggle_close");
 var toggleMenu = document.querySelector(".main-navigation__list");
 
 buttonToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  buttonToggle.classList.toggle("main-navigation__toggle");
+  buttonToggle.classList.toggle("main-navigation__toggle_close");
   toggleMenu.classList.toggle("main-navigation__list_hidden");
 });
