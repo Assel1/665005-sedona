206f05904305eaf7ab638fa33f4dd203da7c129d
Assel1 <assel1@users.noreply.github.com>
2018-05-31 10:16:37 +0600

    финальная правка модификатор toggle

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
-var buttonToggle = document.querySelector(".main-navigation__toggle_close");
+var buttonToggle = document.querySelector(".main-navigation__toggle--close");
 var toggleMenu = document.querySelector(".main-navigation__list");
 
 buttonToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  buttonToggle.classList.toggle("main-navigation__toggle_close");
+  buttonToggle.classList.toggle("main-navigation__toggle--close");
   toggleMenu.classList.toggle("main-navigation__list_hidden");
 });
